5394ce6a307bbd842640512e30f2f7f5bed2d36a
barbora <barbora.celesova@gmail.com>
2017-11-02 21:51:13 +0100

    Adding rules to dictionary

--- mininet-scripts/fdrs.py
+++ mininet-scripts/fdrs.py
@@ -1,7 +1,23 @@
 import sys
 
-def main():
+def add_rule(rulesdict):
+    sourceIP = raw_input('Source IP add: ')
+    destIP = raw_input('Destination IP add: ')
+    action = raw_input('Permit/Deny [P/D]: ')
+    protocol = raw_input('IP/ICMP/TCP/UDP/HTTP:')
 
+    key = (sourceIP,destIP)
+    data = (action,protocol)
+
+    if key not in rulesdict:
+        rulesdict[key] = [data]
+        print('\nRule successfully stored\n')
+    else:
+        print('\nSame rule already exist\n')
+
+
+def main():
+    rulesdict = {}
 
     while(1):
         prepinac = raw_input('Write required command or -h for help\n')
@@ -9,7 +25,8 @@ def main():
         if 'h' in prepinac:
             print ('Select from these commands:\n\t-a\tadd rule to firewall\n\t-r\tremove rule from firewall\n\t-s\tshow all rules\n\t-x\texit\n')
         elif 'a' in prepinac:
-            print ('Follow guide for adding rules')
+            print ('Follow guide for adding rules\n')
+            add_rule(rulesdict)
         elif 'r' in prepinac:
             print ('No rules to be deleted at this point')
         elif 's' in prepinac:
